91b535bf6eee27b0749d6f879a0edda19db886d3
CelesteB3620 <laura.berrio@correounivalle.edu.co>
2025-07-26 18:42:59 -0500

    Register user integration (#7)

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 import api from '../api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {View,Text,Image,TextInput,TouchableOpacity,StyleSheet} from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
 
 export default function LoginScreen({ navigation }) {
-    
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [secure, setSecure] = useState(true)
-    const iconUguee = require('../assets/Uguee.png')
-    const handleLogin = async () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [secure, setSecure] = useState(true)
+  const iconUguee = require('../assets/Uguee.png')
+  const handleLogin = async () => {
+
+    if (!username.trim() || !password.trim()) {
+      alert('Por favor ingresa el usuario y la contraseña.');
+      return;
+    }
 
     try {
       const response = await api.post('/api/login/', {
@@ -23,7 +28,7 @@ export default function LoginScreen({ navigation }) {
         // Navega a la pantalla principal y guarda el token
         await AsyncStorage.setItem('token', response.data.access);
         navigation.navigate('Inicio');
-        
+
       } else {
         alert('Credenciales incorrectas');
       }
@@ -34,128 +39,140 @@ export default function LoginScreen({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
-      <Image source = {iconUguee} style={styles.iconoUguee}/>
-      <View style={{ width: '80%', alignItems: 'flex-start' }}>
-      <Text style={styles.text}>¡Bienvenid@!</Text>
-      <Text style={styles.subtitle}>Tu app de transporte de confianza</Text>
-      <Text style={styles.label}>Usuario</Text>
-      <View style={styles.inputContainer}>
-      <TextInput
-        style={[styles.input, { flex: 1 }]}
-        placeholder="Username o Email"
-        value={username}
-        onChangeText={setUsername}
-        keyboardType="default"
-        autoCapitalize="none" /></View>
-      <Text style={styles.label}>Contraseña</Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={[styles.input, { flex: 1 }]}
-          placeholder="**********"
-          secureTextEntry={secure}
-          value={password}
-          onChangeText={setPassword}/>
-        <TouchableOpacity onPress={() => setSecure(!secure)}>
-          <Ionicons
-            name={secure ? 'eye-off-outline' : 'eye-outline'}
-            size={24}
-            color="#555"
-            style={{ marginRight: 10 }}/>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container} keyboardShouldPersistTaps="handled">
+        <Image source={iconUguee} style={styles.iconoUguee} />
+        <View style={{ width: '80%', alignItems: 'flex-start' }}>
+          <Text style={styles.text}>¡Bienvenid@!</Text>
+          <Text style={styles.subtitle}>Tu app de transporte de confianza</Text>
+          <Text style={styles.label}>Usuario</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={[styles.input, { flex: 1 }]}
+              placeholder="Username o Email"
+              value={username}
+              onChangeText={setUsername}
+              keyboardType="default"
+              autoCapitalize="none" /></View>
+          <Text style={styles.label}>Contraseña</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={[styles.input, { flex: 1 }]}
+              placeholder="**********"
+              secureTextEntry={secure}
+              value={password}
+              onChangeText={setPassword} />
+            <TouchableOpacity onPress={() => setSecure(!secure)}>
+              <Ionicons
+                name={secure ? 'eye-off-outline' : 'eye-outline'}
+                size={24}
+                color="#555"
+                style={{ marginRight: 10 }} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <TouchableOpacity onPress={handleLogin}>
+          <LinearGradient
+            colors={['#340378ff', '#6723c6ff']}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+            style={styles.button} >
+            <Text style={styles.buttonText}>Entrar</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.forgot}>¿Olvidaste tu contraseña?</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+          <Text style={styles.back}>Volver al inicio</Text>
         </TouchableOpacity>
       </View>
-      </View>
-      
-      <TouchableOpacity onPress={handleLogin}>
-        <LinearGradient
-                colors={['#340378ff', '#6723c6ff']}
-                start={{ x: 0, y: 0 }}
-                end={{ x: 0, y: 1 }}
-                style={styles.button} >
-                <Text style={styles.buttonText}>Entrar</Text>
-              </LinearGradient>
-      </TouchableOpacity>
-
-      <TouchableOpacity>
-        <Text style={styles.forgot}>¿Olvidaste tu contraseña?</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-        <Text style={styles.back}>Volver al inicio</Text>
-      </TouchableOpacity>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
-
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center' },
-  iconoUguee: { 
-    width: 300, 
-    height: 300, 
-    resizeMode: 'contain'},
-  text: { 
-    width: 190, 
-    height: 50, 
-    fontFamily: "Quicksand", 
-    fontSize: 30, 
-    fontWeight: '700', 
-    textAlign: "left", 
-    color: "#35424A"},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  iconoUguee: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain'
+  },
+  text: {
+    width: 190,
+    height: 50,
+    fontFamily: "Quicksand",
+    fontSize: 30,
+    fontWeight: '700',
+    textAlign: "left",
+    color: "#35424A"
+  },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     color: '#777',
-    marginBottom: 30},
+    marginBottom: 30
+  },
   label: {
     color: '#8A4AE3',
     fontSize: 16,
     fontWeight: '600',
     marginTop: 10,
-    marginBottom: 4 },
+    marginBottom: 4
+  },
   input: {
-    backgroundColor: '#f9f9f9', 
-    paddingVertical: 10, 
-    paddingHorizontal: 15, 
-    borderRadius: 6, 
-    borderBottomWidth: 1, 
-    borderBottomColor: '#ccc', 
-    marginBottom: 10},
+    backgroundColor: '#f9f9f9',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: 10
+  },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     borderBottomColor: '#ccc',
     borderBottomWidth: 1,
-    backgroundColor: '#f9f9f9', 
-    borderRadius: 6, 
-    paddingRight: 5 },
-  button: { 
-    padding: 15, 
-    borderRadius: 15, 
-    marginTop: 30, 
-    width: 300, 
+    backgroundColor: '#f9f9f9',
+    borderRadius: 6,
+    paddingRight: 5
+  },
+  button: {
+    padding: 15,
+    borderRadius: 15,
+    marginTop: 30,
+    width: 300,
     alignItems: 'center',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.3,
     shadowRadius: 4.65,
-    elevation: 8,},
-  buttonText: { 
-    color: '#fff', 
-    fontSize: 20 },  
+    elevation: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20
+  },
   forgot: {
     textAlign: 'center',
     color: '#888',
     fontSize: 15,
     marginBottom: 25,
-    marginTop:10},
+    marginTop: 10
+  },
   back: {
     textAlign: 'center',
     fontSize: 15,
     color: '#8A4AE3',
     fontWeight: '600',
-    textDecorationLine: 'underline'},
+    textDecorationLine: 'underline'
+  },
 });
--- screens/PasajeroScreen.js
+++ screens/PasajeroScreen.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity, ScrollView, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { Picker } from '@react-native-picker/picker';
+import { USER_TYPES, PASSENGER_TYPES } from '../constants'
+import api from '../api';
+import { ADMIN_USERNAME, ADMIN_PASSWORD } from '@env';
 
 export default function PasajeroScreen() {
   const navigation = useNavigation();
@@ -12,7 +15,86 @@ export default function PasajeroScreen() {
   const [documento, setDocumento] = useState('');
   const [correo, setCorreo] = useState('');
   const [contrasena, setContrasena] = useState('');
-  const [tipoUsuario, setTipoUsuario] = useState('');
+  const [direccion, setDireccion] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [codigo, setCodigo] = useState('');
+  const [selectedCollege, setSelectedCollege] = useState(null);
+  const [tipoPasajero, setTipoPasajero] = useState(null);
+  const [universidades, setUniversidades] = useState([]);
+  const isValidEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const handleRegister = async () => {
+    if (!nombre || !apellido || !documento || !correo || !contrasena || !direccion || !telefono || !selectedCollege || !tipoPasajero) {
+      alert('Por favor completa todos los campos.');
+      return;
+    }
+    if (!esEmailValido(correo)) {
+      alert('Por favor ingresa un correo electrónico válido.');
+      return;
+    }
+
+    try {
+      const response = await api.post('/api/users/', {
+        username: `${nombre}${apellido}`,
+        email: correo,
+        personal_id: documento,
+        address: direccion,
+        phone: telefono,
+        code: codigo,
+        user_type: USER_TYPES.PASSENGER,
+        passenger_type: tipoPasajero,
+        college: selectedCollege,
+        password: contrasena,
+        attachments: []
+      });
+
+      if (response.status === 201) {
+        alert('Registro exitoso. Ahora puedes iniciar sesión.');
+        navigation.navigate('Login');
+      }
+
+    } catch (error) {
+      if (error.response) {
+        console.error('Backend respondió con error:', error.response.data);
+      } else if (error.request) {
+        console.error('No hubo respuesta del servidor:', error.request);
+      } else {
+        console.error('Error al configurar la petición:', error.message);
+      }
+      alert('Error al registrarse');
+    }
+  };
+
+  useEffect(() => {
+    const fetchTokenAndColleges = async () => {
+      try {
+        const loginResponse = await api.post('/api/login/', {
+          username: ADMIN_USERNAME,
+          password: ADMIN_PASSWORD,
+        });
+
+        const admin_token = loginResponse.data.access;
+
+        const collegeResponse = await api.get('/api/colleges/', {
+          headers: {
+            Authorization: `Bearer ${admin_token}`,
+          },
+        });
+
+        if (collegeResponse.status === 200) {
+          setUniversidades(collegeResponse.data);
+        }
+      } catch (error) {
+        console.error('Error al obtener universidades:', error.response?.data || error.message);
+        alert('No se pudieron cargar las universidades.');
+      }
+    };
+
+    fetchTokenAndColleges();
+  }, []);
 
   return (
     <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
@@ -51,6 +133,8 @@ export default function PasajeroScreen() {
         keyboardType="email-address"
         value={correo}
         onChangeText={setCorreo}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
       <TextInput
         style={styles.input}
@@ -61,11 +145,50 @@ export default function PasajeroScreen() {
         onChangeText={setContrasena}
       />
 
-      <Text style={styles.label}>¿Qué eres?</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Dirección"
+        placeholderTextColor="#aaa"
+        value={direccion}
+        onChangeText={setDireccion}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Teléfono"
+        placeholderTextColor="#aaa"
+        keyboardType="phone-pad"
+        value={telefono}
+        onChangeText={setTelefono}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Código"
+        placeholderTextColor="#aaa"
+        value={codigo}
+        onChangeText={setCodigo}
+      />
+
+      <Text style={styles.label}>Universidad</Text>
       <View style={styles.pickerContainer}>
         <Picker
-          selectedValue={tipoUsuario}
-          onValueChange={(itemValue) => setTipoUsuario(itemValue)}
+          selectedValue={selectedCollege}
+          onValueChange={(itemValue) => setSelectedCollege(itemValue)}
+          style={Platform.OS === 'ios' ? styles.pickerIOS : styles.pickerAndroid}
+        >
+          <Picker.Item label="Selecciona una universidad" value={null} />
+          {universidades.map((college) => (
+            <Picker.Item key={college.college_id} label={college.name} value={college.college_id} />
+          ))}
+        </Picker>
+      </View>
+
+      <Text style={styles.label}>Perfil Universitario</Text>
+      <View style={styles.pickerContainer}>
+        <Picker
+          selectedValue={tipoPasajero}
+          onValueChange={(itemValue) => setTipoPasajero(itemValue)}
           style={Platform.select({
             ios: styles.pickerIOS,
             android: styles.pickerAndroid,
@@ -73,20 +196,20 @@ export default function PasajeroScreen() {
           dropdownIconColor="#1A0A1F"
         >
           <Picker.Item label="Selecciona una opción..." value="" />
-          <Picker.Item label="Estudiante" value="Estudiante" />
-          <Picker.Item label="Profesor" value="Profesor" />
-          <Picker.Item label="Administrativo" value="Administrativo" />
+          <Picker.Item label="Estudiante" value={PASSENGER_TYPES.STUDENT} />
+          <Picker.Item label="Profesor" value={PASSENGER_TYPES.PROFESSOR} />
+          <Picker.Item label="Administrativo" value={PASSENGER_TYPES.STAFF} />
         </Picker>
       </View>
 
-      <TouchableOpacity style={styles.registerButton} onPress={() => {/* Acción al registrarse */}}>
+      <TouchableOpacity style={styles.registerButton} onPress={handleRegister}>
         <Text style={styles.registerButtonText}>Registrarse</Text>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.loginLink} onPress={() => navigation.navigate('Login')}>
         <Text style={styles.loginLinkText}>Tengo una cuenta</Text>
       </TouchableOpacity>
- 
+
 
     </ScrollView>
   );
@@ -191,14 +314,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignSelf: 'center',
     marginTop: 10,
-},
+  },
 
   loginLinkText: {
-   color: '#0167FF',
-   fontSize: 14,
-   fontWeight: 'bold',
-   textDecorationLine: 'none',
-},
-
+    color: '#0167FF',
+    fontSize: 14,
+    fontWeight: 'bold',
+    textDecorationLine: 'none',
+  },
 
 });
